dd1555dd69450389d626a7aabd62f0d1cc7d7839
Alex Dunne <hi@alexdunne.net>
2020-08-12 00:29:55 +0100

    Added the basics of the GameInstanceProcess GenServer

--- lib/geo_pong/game_instances/game_instance_process.ex
+++ lib/geo_pong/game_instances/game_instance_process.ex
@@ -1,2 +1,32 @@
 defmodule GeoPong.GameInstances.GameInstanceProcess do
+  use GenServer, restart: :transient
+
+  require Logger
+
+  alias GeoPong.GameInstances.GameInstance
+
+  def start_link(%GameInstance{} = game_instance) do
+    GenServer.start_link(__MODULE__, game_instance,
+      name: {:via, Registry, {GeoPong.GameInstanceRegistry, game_instance.id}}
+    )
+  end
+
+  @impl true
+  def init(%GameInstance{} = game_instance) do
+    {:ok, game_instance}
+  end
+
+  # Callbacks
+
+  @impl true
+  def handle_call(:fetch, _from, %GameInstance{} = state) do
+    {:reply, state, state}
+  end
+
+  # Client API
+
+  def fetch(pid) do
+    pid
+    |> GenServer.call(:fetch)
+  end
 end
